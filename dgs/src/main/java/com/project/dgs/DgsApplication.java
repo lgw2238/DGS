881618877e9abe83729cb4c0eadd771a27d99469
lgw2238 <nihao3634@naver.com>
2023-08-14 17:04:24 +0900

    initial commit 2

---
+++ dgs/src/main/java/com/project/dgs/DgsApplication.java
@@ -0,0 +1,13 @@
+package com.project.dgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgsApplication.class, args);
+	}
+
+}
